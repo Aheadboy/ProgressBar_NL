d1a89fbe5e30f7a5732852a7c4f3338d478e050a
fhp <736054836@qq.com>
2017-09-20 10:59:15 +0800

    Initial commit

---
+++ app/src/main/java/com/ljj1/a73605/progressbar_nl/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ljj1.a73605.progressbar_nl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
